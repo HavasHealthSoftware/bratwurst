00797f7aebc2ba8aaa9485233261b503a229f9dc
Stephen Willcock <steve@rowanbeach.com>
2013-08-12 13:48:37 +0100

    refactor, now supports multiple environments, not just two

--- index.js
+++ index.js
@@ -18,168 +18,60 @@
 
 */
 
-var connect = require('connect'),
-	render = require('connect-render'),
-	fs = require('fs'),
-	path = require('path'),
+var path = require('path'),
 	Gaze = require('gaze').Gaze,
 	url = require('url'),
-	proxy = require('proxy-middleware'),
-	argv = require('optimist').argv,
 	winston = require('winston'),
-	port = argv.port || 9020,
-	locale = argv.locale,
-	configBuilder = require('./configBuilder'),
-	apiUrl = argv.apiurl || 'http://localhost:9000/api',
-	localeBasePath = 'locale',
-	logLevel = argv.loglevel || 'info';
+	program = require('commander'),
+	BratwurstServer = require('./lib/bratwurst-server'),
+	bratwurstBuild = require('./lib/build')
+	util = require('util');
 
-
-winston.config.npm.colors.debug = winston.config.npm.colors.info;
-winston.config.npm.colors.info = 'magenta';
-winston.setLevels(winston.config.npm.levels);
-winston.addColors(winston.config.npm.colors);
-winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {
-	colorize: true,
-	level: logLevel
-});
-
-if (!locale) {
-	winston.error('You must specify a locale');
-	process.exit(1);
+// Setup logger
+function setupLogger(logLevel){
+	winston.config.npm.colors.debug = winston.config.npm.colors.info;
+	winston.config.npm.colors.info = 'magenta';
+	winston.setLevels(winston.config.npm.levels);
+	winston.addColors(winston.config.npm.colors);
+	winston.remove(winston.transports.Console);
+	winston.add(winston.transports.Console, {
+		colorize: true,
+		level: logLevel
+	});
 }
 
-locale = locale.toLowerCase();
+program
+	.version('0.0.6')
 
-var environments = [localeBasePath + '/' + locale, 'app'];
-var rootPath = process.cwd() + '/';
+var commandExecuted = false;
 
-winston.info('Starting site for ' + locale + ' on port ' + port);
-
-var pageData;
-
-var getJsonFiles = function() {
-
-	configBuilder.getJsonFiles('/app', 'views/', {}, function(err, coreData) {
-
-		winston.info('Core json data read');
-
-		// Get json data overrides
-		configBuilder.getJsonFiles('/locale/' + locale, 'views/', coreData, function(err, mergedData) {
-			winston.info('Locale specific json data read');
-			pageData = mergedData;
-			pageData['/index.json'] = {}; // temp measure to ensure root index is processed (can't currently read json files at this level as it hits node_modules)
-		});
-
+var serverCommand = program.command('server')
+	.description('run dev server')
+	.option('-e, --environments <environments>', 'Specify the environment paths in a comma separated list')
+	.option('-l, --locale <locale>', 'Specify the locale')
+	.option('-p, --port [port]', 'Port [9020]', 9020)
+	.option('-b, --base-path [basePath]', 'Base Path [./]', './')
+	.option('-a, --api-url [apiUrl]', 'Url to the api server [http://localhost:9000/api]', 'http://localhost:9000/api')
+	.option('-g, --log-level [level]', 'Set the loglevel [info]', 'info')
+	.action(function() {
+	commandExecuted = true;
+	setupLogger(serverCommand.logLevel);
+	var server = new BratwurstServer(serverCommand);
+	server.on('usageIssue', function() {
+		serverCommand.help();
 	});
+	server.start();
+});
 
+program.command('build')
+	.description('run build pipeline')
+	.option('-e, --environments <environments>', 'Specify the environment paths in a comma separated list')
+	.action(function() {
+	console.log('build');
+});
+
+program.parse(process.argv);
+
+if (!commandExecuted) {
+	program.help();
 }
-
-getJsonFiles();
-
-var gazeTargets = environments.map(function(env) {
-	var t = path.join(rootPath, env, '/**/*.json');
-	winston.debug('gaze target: ' + t);
-	return t;
-});
-
-var gaze = new Gaze(gazeTargets);
-
-// Files have all started watching
-gaze.on('ready', function(watcher) {
-	winston.info('Watching json files...');
-});
-
-// A file has been added/changed/deleted has occurred
-gaze.on('all', function(event, filepath) {
-	winston.info('File changed: ' + filepath);
-	getJsonFiles();
-});
-
-
-var connectApp = connect(
-	render({
-	root: rootPath,
-	layout: false,
-	cache: false, // false for debug
-	helpers: {
-		locale: locale
-	}
-}));
-
-connectApp.use('/api', proxy(url.parse(apiUrl)));
-
-var pathWithDefaultDocument = function(urlString) {
-	if (urlString.substring(urlString.length - 1) === '/') {
-		return urlString + 'index.html';
-	}
-	return urlString;
-};
-
-var useTemplating = function(requestUrl) {
-	return requestUrl.indexOf('.html') != -1;
-}
-
-// Set up EJS Connect middleware for each environment (currently we just have two)
-environments.forEach(function(env) {
-
-	connectApp.use(function(req, res, next) {
-
-		var requestUrl = pathWithDefaultDocument(req.url).toLowerCase();
-
-		if (!useTemplating(requestUrl)) {
-			winston.debug(requestUrl + ' not templatable for ' + env + ', skipping');
-			next();
-			return;
-		};
-
-		var lookupUrl = requestUrl.replace('.html', '.json');
-
-		var fullVirtualPath = '/' + env + pathWithDefaultDocument(req.url);
-		winston.debug(fullVirtualPath);
-		var fullPath = path.normalize(rootPath + fullVirtualPath);
-
-
-		fs.exists(fullPath, function(exists) {
-
-			if (exists) {
-				winston.debug(fullVirtualPath + ' found in ' + env + ' directory');
-				var jsonData = pageData[lookupUrl];
-
-				if (jsonData) {
-					winston.debug('Found data using ' + lookupUrl);
-					res.render(fullVirtualPath, {
-						pageData: jsonData
-					});
-				} else {
-					winston.debug('No data found for ' + lookupUrl);
-					next();
-				}
-
-			} else {
-
-				winston.debug(fullVirtualPath + ' not found in ' + env + ' directory');
-				next();
-
-			}
-
-		});
-
-
-
-	});
-
-});
-
-environments.forEach(function(env) {
-
-	connectApp.use(connect.static(process.cwd() + '/' + env), {
-		maxAge: 0
-	});
-
-});
-
-connectApp.listen(port);
-
-winston.info('Listening on port ' + port);
